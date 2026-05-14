29bb964f86623ee58caae2e00e57ec0b1756728a
JensenRasmus <rasmusj@imada.sdu.dk>
2026-05-14 13:19:45 +0200

    Update

--- figures/plotData_Fig_2.m
+++ figures/plotData_Fig_2.m
@@ -1,74 +1,33 @@
 clear all
 close all
 clc
-rtimes = readNPY('runtimes.npy');
-ptimes = readNPY('processtimes.npy');
 
+%str = "err_coord_approx_mode1";
+str = "err_coord_approx_mode1_p400";
+errs = readNPY(str+'.npy')';
 
-labs = {'PF','PL','PL Cay','QG St.','QG St. Cay.','QG Gr.','QR','Cayley'};
-figure
-% Ensure rtimes and ptimes have same number of rows
-n = max(size(rtimes,1), size(ptimes,1));
-% If one has fewer rows, pad with NaNs
-if size(rtimes,1) < n
-    rtimes(end+1:n, :) = NaN;
-end
-if size(ptimes,1) < n
-    ptimes(end+1:n, :) = NaN;
-end
+labs = {'Exp','PF','PL'};
+colors_r = {[0    0    0],[1 0 0],[0 0 1]};
+linestyles = {'-','--',':'};
 
-% Number of columns for each
-cr = size(rtimes,2);
-cp = size(ptimes,2);
+xla = 't';
+yla = 'Error';
 
-% Choose distinct colors and markers
-% colors_r = lines(cr);
-% colors_p = lines(cp);
-% colors_r = {[1 0 0],[0 0 1],[0.5210    0.0860    0.8190],[0.9290    0.6940    0.1250]}
-cs = lines(8);
-cs = cs(5:end,:);
-colors_r = {[1 0 0],[0 0 1],[0.7520    0.3600    0.9840],cs(1,:),cs(2,:),cs(3,:),cs(4,:),[0 0 0]};
-
-markers = {'o','s','d','^','p','h','x','*','+','*','v','>','<'}; % cycle if more cols than markers
-% Provide a list of common MATLAB line styles for reference
-linestyles = {'-','--','-','--','-','--','-','--'};
-
-xla = 'p';
-yla = 'Runtime (s)';
-% Subplot for rtimes
-subplot(1,2,1)
+idx = linspace(0,1,size(errs,2));
 hold on
-idx = [500 1000 1500 2000];
-for k = 1:cr
-    m = markers{mod(k-1,numel(markers))+1};
+for k = 1:3
     ls = linestyles{mod(k-1,numel(linestyles))+1};
-    plot(idx, rtimes(:,k), [ls m], 'Color', colors_r{k}, 'MarkerFaceColor', colors_r{k},'LineWidth',2);
+    plot(idx,errs(k,:),ls,'Color', colors_r{k}, 'MarkerFaceColor', colors_r{k},'LineWidth',2)
 end
 hold off
-title('Interpolation')
 xlabel(xla)
 ylabel(yla)
+ylim([-0.0001,max(errs(:))])
 legend(labs,'Location','northwest')
 grid on
-
-% Subplot for ptimes
-subplot(1,2,2)
-hold on
-for k = 1:cp
-    m = markers{mod(k-1,numel(markers))+1};
-    ls = linestyles{mod(k-1,numel(linestyles))+1};
-    plot(idx, ptimes(:,k), [ls m], 'Color', colors_r{k}, 'MarkerFaceColor', colors_r{k},'LineWidth',2);
-end
-hold off
-title('Preprocessing')
-xlabel(xla)
-ylabel(yla)
-legend(labs,'Location','northwest')
-grid on
-% Set figure size to 1200x600 pixels (width x height)
 fig = gcf;
 fig.Units = 'pixels';
-fig.Position(3:4) = [1200 600];
+fig.Position(3:4) = [800 400];
 % Increase font sizes for all axes, legends, and text in the figure
 fs = 20; % desired base font size
 ax = findall(fig,'Type','axes');
@@ -88,4 +47,4 @@ for t = 1:numel(txt)
     txt(t).FontSize = fs;
 end
 % Save the figure as a PNG file
-saveas(fig, 'runtime_processing_plot.png');
+saveas(fig, str+'.png');
--- figures/plotData_Fig_3.m
+++ figures/plotData_Fig_3.m
@@ -1,31 +1,73 @@
-clear all
 close all
-clc
+timings_p_retracton = readNPY('timings_p_retractions_large.npy')';
+timings_p_retracton = timings_p_retracton;
 
-str = "errs_n_1000_p_400";
-errs = readNPY(str+'.npy');
+% Color standard
+% Red: PF
+% Blue: PL
+% 
 
-labs = {'PF','PL'};
-colors_r = {[1 0 0],[0 0 1]};
-linestyles = {'-','--'};
+figure
+labs = {'PF','PF inv','PL','PL inv.','PL Cay.','PL Cay. inv','Cayley', 'Cayley inv'};
+subplot(1,2,1)
+% Plot each column with a distinct line style and add legend/labels
+styles = {'-','--','-','--','-','--','-','--'};
+markers = {'o','o','s','s','*','*'};
 
-xla = 't';
-yla = 'Error';
 
-idx = linspace(0,1,size(errs,2));
+colors = {[1 0 0],[1 0 0],[0 0 1],[0 0 1],[0.7520    0.3600    0.9840],[0.7520    0.3600    0.9840], [0 0 0],[0 0 0]};
+
+%colors = lines(size(timings_p_retracton,2));
 hold on
-for k = 1:2
-    ls = linestyles{mod(k-1,numel(linestyles))+1};
-    plot(idx,errs(k,:),ls,'Color', colors_r{k}, 'MarkerFaceColor', colors_r{k},'LineWidth',2)
+%h = gobjects(1,size(timings_p_retracton,2));
+x = linspace(500,2000,size(timings_p_retracton,1));
+for k = 1:size(timings_p_retracton,2)
+    style = styles{mod(k-1,numel(styles))+1};
+    mark = markers{mod(k-1,numel(markers))+1};
+    h(k) = plot(x, timings_p_retracton(:,k), 'LineStyle', style,'Marker',mark,'Color', colors{k}, 'LineWidth', 2);
 end
 hold off
-xlabel(xla)
-ylabel(yla)
-legend(labs,'Location','northwest')
+xlabel('p');
+ylabel('Time (s)');
+title('Individual timing results');
+legend(labs, 'Location', 'northwest');
 grid on
+
+
+% Add up combinded cost
+
+timings_combinded = zeros(4,4);
+timings_combinded(1,:) = timings_p_retracton(:,1)' + timings_p_retracton(:,2)';
+timings_combinded(2,:) = timings_p_retracton(:,3)' + timings_p_retracton(:,4)';
+timings_combinded(3,:) = timings_p_retracton(:,5)' + timings_p_retracton(:,6)';
+timings_combinded(4,:) = timings_p_retracton(:,7)' + timings_p_retracton(:,8)';
+
+timings_combinded = timings_combinded';
+
+subplot(1,2,2)
+styles = {'-','--',':','-.'};
+markers = {'o','s','*','d'};
+colors = {[1 0 0],[0 0 1],[0.7520    0.3600    0.9840],[0 0 0]};
+
+hold on
+x = linspace(500,2000,size(timings_p_retracton,1));
+for k = 1:size(timings_combinded,2)
+    style = styles{mod(k-1,numel(styles))+1};
+    mark = markers{mod(k-1,numel(markers))+1};
+    h(k) = plot(x, timings_combinded(:,k), 'LineStyle', style,'Marker',mark,'Color', colors{k}, 'LineWidth', 2);
+end
+
+labs = {'PF + PF inv','PL + PL inv.','PL Cay. + PL Cay. inv','Cayley + Cayley inv'};
+xlabel('p');
+ylabel('Time (s)');
+title('Forward+inverse timing results');
+legend(labs, 'Location', 'northwest');
+grid on
+
+% Set figure size to 1200x600 pixels (width x height)
 fig = gcf;
 fig.Units = 'pixels';
-fig.Position(3:4) = [800 400];
+fig.Position(3:4) = [1200 600];
 % Increase font sizes for all axes, legends, and text in the figure
 fs = 20; % desired base font size
 ax = findall(fig,'Type','axes');
@@ -45,4 +87,4 @@ for t = 1:numel(txt)
     txt(t).FontSize = fs;
 end
 % Save the figure as a PNG file
-saveas(fig, str+'.png');
+saveas(fig, 'timings.png');
